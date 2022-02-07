a75ea0ae87278e02e4a26061d01c799e61c38a13
Stanisław Dera <stanislawdera@outlook.com>
2022-02-07 13:05:19 +0100

    Update theme

--- styles/theme.js
+++ styles/theme.js
@@ -5,8 +5,18 @@ export const theme = {
     lg: "15px",
     xl: "20px",
   },
+  fontSizes: {
+    sm: "18px",
+    md: "24px",
+    lg: "36px",
+    xl: "48px",
+  },
   media: {
     tablet: "@media (min-width: 728px)",
     desktop: "@media (min-width: 1024px)",
+  },
+  colors: {
+    primary: "#833AB4",
+    light: "#DFCEEC",
   },
 };
